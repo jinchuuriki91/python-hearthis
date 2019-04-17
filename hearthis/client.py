2ad2dde9330440885891a7051b52860978b37584
Gandhar Pednekar <gandhar.pednekar15@gmail.com>
2019-04-17 20:48:23 +0530

    User History, Search, Login and Logout API

--- hearthis/client.py
+++ hearthis/client.py
@@ -1,15 +1,29 @@
 # Python imports
 import json
-from requests import request
+from requests import Session
 
 
 class Hearthis(object):
 
-    def __init__(self, requests_timeout=None):
+    SEARCH_TYPE = ["tracks", "user", "playlists"]
+
+    def __init__(self, email=None, password=None, requests_timeout=None, requests_session=None):
         self.base_url = "https://api-v2.hearthis.at"
+        self.is_logged_in = False
         self.default_page = 1
         self.default_count = 5
+        self.default_duration = 5  # 5 minutes
         self.requests_timeout = requests_timeout
+        self._user_meta = dict()
+
+        if isinstance(requests_session, Session):
+            self._session = requests_session
+        else:
+            self._session = Session()
+
+        if email and password:
+            self.login(email, password)
+            self.is_logged_in = True
 
     def _request(self, method, url, payload=None, params=None):
         kwargs = dict()
@@ -17,16 +31,11 @@ class Hearthis(object):
         if not url.startswith("http"):
             url = self.base_url + url
 
-        headers = {
-            "Content-Type": "application/json"
-        }
-
         if params:
-            kwargs = dict(params=params)
+            kwargs["params"] = params
         if payload:
-            kwargs["data"] = json.dumps(payload)
-
-        req = request(method, url, headers=headers, **kwargs)
+            kwargs["data"] = payload
+        req = self._session.request(method, url, **kwargs)
 
         try:
             req.raise_for_status()
@@ -35,7 +44,45 @@ class Hearthis(object):
         finally:
             req.connection.close()
 
+        print(req.request.url)
+        print(req.request.body)
         return req.json()
+
+    def login(self, email, password):
+        payload = {
+            "email": email,
+            "password": password
+        }
+        resp = self._request("POST", "/login/", payload=payload)
+        if "success" in resp:
+            raise Exception(resp["message"])
+        self._user_meta = resp
+        return resp
+
+    def logout(self):
+        return self._request("GET", "/logout/")
+
+    def search(
+        self, query, type="tracks", page=None, count=None, duration=None):
+
+        if type not in self.SEARCH_TYPE:
+            raise Exception("Invalid search type")
+        params = {
+            "t": query,
+            "type": type,
+            "page": page if page else self.default_page,
+            "count": count if count else self.default_count,
+            "duration": duration if duration else self.default_duration
+        }
+        return self._request("GET", "/search", params=params)
+
+    def user_history(self, page=None, count=None):
+        if not self.is_logged_in:
+            raise Exception("Log in required")
+        params = dict()
+        params["page"] = page if page else self.default_page
+        params["count"] = count if count else self.default_count
+        return self._request("GET", "/v2.2/htry/", params=params)
 
     def all_genres(self):
         return self._request("GET", "/categories/")
@@ -44,10 +91,8 @@ class Hearthis(object):
         params = dict()
         params["page"] = page if page else self.default_page
         params["count"] = count if count else self.default_count
-        if duration:
-            params["duration"] = duration
-
-        return self._request("GET", "/categories/%s/" % genre, payload=params)
+        params["duration"] = duration if duration else self.default_duration
+        return self._request("GET", "/categories/%s/" % genre, params=params)
 
     def single_artist(self, artist):
         return self._request("GET", "/%s/" % artist)
